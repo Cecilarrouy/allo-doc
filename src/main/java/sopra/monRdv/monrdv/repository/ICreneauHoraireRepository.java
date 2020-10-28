141a8fd67fd290f5590e17f423d4eda766ea704f
Brice <brice.lohrer@gmail.com>
2020-10-28 15:56:10 +0100

    angular

--- src/main/java/sopra/monRdv/monrdv/repository/ICreneauHoraireRepository.java
+++ src/main/java/sopra/monRdv/monrdv/repository/ICreneauHoraireRepository.java
@@ -1,9 +1,19 @@
 package sopra.monRdv.monrdv.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import sopra.monRdv.monrdv.model.CreneauHoraire;
+import sopra.monRdv.monrdv.model.LieuDeConsultation;
 
 public interface ICreneauHoraireRepository  extends JpaRepository<CreneauHoraire, Long>{
-
+	
+	//affichage des creneaux horaires par praticien et par lieu de consultation
+//			@Query("select cre from CreneauHoraire cre where cre.praticien = :praticien and cre.lieu = :lieu")
+//			List<CreneauHoraire> findCreneauHoraireByPraticienAndLieu(@Param("phase") CreneauHoraire creneauHoraire, @Param("lieu") LieuDeConsultation lieu );			
+			
 }
